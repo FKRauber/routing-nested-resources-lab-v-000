155f67d34f24eb99bc8fb5915c75a65fcf9f3345
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-09 22:56:12 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,9 +16,9 @@ class SongsController < ApplicationController
   def show
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
-      
+      @song = @artist.songs.find_by(id: params[:id])
       if condition
-        
+
       end
     else
       @song = Song.find(params[:id])
