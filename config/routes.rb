1260a2b13c8ada190d977e84ba331f38108866f8
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-09 22:48:47 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:index, :show, :new, :create, :edit, :update] do
-    resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+  resources :artists do
+    resources :songs, only: [:index, :show]
   end
 
 end
