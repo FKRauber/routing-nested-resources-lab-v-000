1f81450d7d7dd61c00d3abad57dfb0ee4468f3e3
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-09 22:54:27 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,6 +3,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist == nil
+        flash[:alert] = "Artist Not Found!"
         redirect_to artists_path
       else
         @songs = @artist.songs
