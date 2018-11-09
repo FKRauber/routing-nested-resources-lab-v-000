a3e180048b2835b923e53458b632ea99c8df5539
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-09 22:53:03 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,15 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if condition
+
+      else
+
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
@@ -50,4 +59,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
